6bc71fe20b757b9597ed32f246c466ddd64b6e65
ericweidman <ericweidman@gmail.com>
2016-02-29 11:52:34 -0500

    Initial import

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,9 +1,6 @@
 package com.theironyard;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
 
@@ -20,10 +17,26 @@ public class Main {
 
         //THIS IS THE WRONG WAY
         //String name = "Alice";
-        String name = "', 10.0, true, 50); DROP TABLE players; --";
-        stmt.execute(String.format("INSERT INTO players VALUES (Null, '%s', 10.0, true, 50)", name));
+        //String name = "', 10.0, true, 50); DROP TABLE players; --";
+        //stmt.execute(String.format("INSERT INTO players VALUES (Null, '%s', 10.0, true, 50)", name));
 
 
+        //THE CORRECT WAY
+        PreparedStatement stmt2 = conn.prepareStatement("INSERT INTO players VALUES (NULL, ?, ?, true, 50)");
+        stmt2.setString(1, "Alice");
+        stmt2.setDouble(2, 10.0);
+        stmt2.execute();
+
+
+        ResultSet results = stmt.executeQuery("SELECT * FROM players");
+        while (results.next()){
+            String name = results.getString("name");
+            double health = results.getDouble("health");
+            int score = results.getInt("score");
+            System.out.printf("%s, %s, %s\n", name, health, score);
+
+        }
+        stmt.execute("DROP TABLE players");
         conn.close();
 
 
